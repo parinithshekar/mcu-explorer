d1aee7177738410bcfb912f2d8e9f177c2547bd1
MJM <m.jungmuller@gmail.com>
2021-12-11 15:21:36 +0100

    rebase modifs 2

--- src/AwardBudgetGrossChart.js
+++ src/AwardBudgetGrossChart.js
@@ -1,13 +1,14 @@
-parallelChart = ParallelCoord(JSON_FILE_COOL, {
-    x: (d) => Object.keys(JSON_FILE_COOL),
-    y: (d) => {
-        Y = []
-        for (o in Object.values(JSON_FILE_COOL)){
-            Y.push(Object.keys);
-        }
-        return Y
-    }
-    
-});
 
-document.getElementById('parallel-chart').appendChild(parallelChart);
+data_list = [
+    {budget: '1000000', gross_income: '3000000', awards: '10'},
+    {budget: '1500000', gross_income: '1500000', awards: '2'},
+    {budget: '360000', gross_income: '800000', awards: '8'}
+ ];
+
+console.log("Kilroy started the parallel");
+
+parallelChart = ParallelCoordv3(data_list,{});
+console.log("Kilroy saw the parallel");
+console.log(parallelChart);
+
+document.getElementById('parallel-coo-chart').appendChild(parallelChart);
